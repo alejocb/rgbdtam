9a266a05bf6e1bd47e9b4f272e1d39a1112eb394
Alejo Concha <alejocb@google.com>
2017-03-17 16:23:30 +0100

    Add files via upload

---
+++ build_thirdparty.sh
@@ -0,0 +1,24 @@
+cd ThirdParty/DBoW2
+rm -rf build
+rm -rf lib
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+
+cd ../../g2o
+rm -rf build
+rm -rf lib
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+
+cd ../../segment
+cd ThirdParty/segment
+rm segment
+make
+
+
